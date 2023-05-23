9162957967d2fd0b60a655f72c8c650040b4a504
stephan.keller <stephan.keller@saskia.de>
2023-05-23 08:07:08 +0200

    Initialer Commit

---
+++ Buttons/Wisej.Buttons/Page1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Wisej.Web;
+
+
+namespace Wisej.Buttons
+{
+	public partial class Page1 : Page
+	{
+		public Page1()
+		{
+			InitializeComponent();
+		}
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (this.button11.ImageKey != this.imageList1.Images[1].Name)
+            {
+				this.button11.ImageKey = this.imageList1.Images[1].Name;
+            }
+            else
+            {
+                this.button11.ImageKey = this.imageList1.Images[0].Name;
+            }
+
+			
+
+		}
+    }
+}
